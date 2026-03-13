6097bf18bbe1ac0356f2c69f6541b0bd8852daf5
Mohamed Hakem <mohamedhakem959628@gmail.com>
2026-03-13 04:19:23 +0200

    Switch Redis client from @upstash/redis to ioredis

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,3 @@
-import { Redis } from '@upstash/redis';
+import Redis from 'ioredis';
 
-export const redis = new Redis({
-  url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN
-});
+export const redis = new Redis(process.env.KV_REST_API_URL);
