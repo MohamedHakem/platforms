da875174330f409ea82d3386e29ade3982623771
MohamedHakem <mohamedhakem959628@gmail.com>
2026-03-14 17:56:26 +0200

    feat(config): set output to standalone for improved deployment for docker environment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   // Enable experimental features if needed
   experimental: {
     // Enable filesystem caching for `next dev`
